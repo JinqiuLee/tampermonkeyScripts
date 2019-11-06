a3f14989886d9b01656561cb651c383f035ae851
Jinqiu_Lee <31494327@qq.com>
2019-11-06 17:11:46 +0800

    修复在抢一次券之后第二次抢券不再执行的bug 原理是抢一次券后刷新页面

--- WebRoot/WEB-INF/script/JD.js
+++ WebRoot/WEB-INF/script/JD.js
@@ -1,40 +1,36 @@
 // ==UserScript==
 // @name         京东抢券
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  designed for get 11.11 to 2000-1111RMB
+// @version      0.0.2
+// @description  designed for getting 11.11 2000-1111 RMB
 // @author       Jinqiu.Lee
-// @match        https://pro.jd.com/mall/active/4VkT6xwambh1JFV5zkXKPovyG1tz/index.html
+// @match        https://pro.jd.com/*
 // @require      http://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
 // @grant        none
 // @icon         https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike116%2C5%2C5%2C116%2C38/sign=57d5eca73f2ac65c73086e219a9bd974/7dd98d1001e939014c6dbdf971ec54e737d1968d.jpg
 // ==/UserScript==
 
-var hours, minutes, seconds;
+var hours, minutes, seconds, t1;
+var count = 0;
+
+function control() {
+	if ((hours == 0 || hours == 24 || hours == 10 || hours == 15 || hours == 20)
+			&& minutes == 0 && seconds == 0) {
+		$(".hot_space:eq(0)").click();
+		// 点击后使定时函数失效，防止多次点击，
+		window.clearInterval(t1);
+		// 刷新页面使定时函数再次生效
+		window.location.reload();
+	}// else
+	// console.log("第"+(++count)+"次监控");
+}
 function nowTime() {
 	hours = new Date().getHours();
 	minutes = new Date().getMinutes();
 	seconds = new Date().getSeconds();
 }
-
-function control() {
-	if (hours == 15 && minutes == 33 && seconds == 59) {
-		$(".hot_space:eq(0)").click();
-		sleep(2000);
-	} else
-		console.log("时间没有到");
-}
 function check() {
 	nowTime();
 	control();
 }
-setInterval(check, 0);
-function sleep(n) {
-	var start = new Date().getTime();
-	while (true) {
-		if (new Date().getTime() - start > n) {
-			break;
-		}
-	}
-	
-}
+t1 = window.setInterval(check, 0);
